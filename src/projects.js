5342e3ea05c7c1d8adc49b0296c9a4abe4beb32f
Seung Joo <psjoo3515@gmail.com>
2024-02-17 18:38:52 +0900

    category .js

--- src/projects.js
+++ src/projects.js
@@ -1,2 +1,22 @@
 'use strict';
 
+// category
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project')
+
+categories.addEventListener('click', (event)=>{
+    console.log(event);
+    const filter = event.target.dataset.category;
+    if(filter == null){
+        return;
+    }
+    console.log(filter);
+    projects.forEach(project => {
+        console.log(project.dataset.type);
+        if(filter === 'all' || filter == project.dataset.type){
+            project.style.display = 'block';
+        } else {
+            project.style.display = 'none';
+        }
+    })
+});

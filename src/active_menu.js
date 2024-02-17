423aed91f54027d7b1f1d43a44db06e181bdc810
Seung Joo <psjoo3515@gmail.com>
2024-02-18 00:28:47 +0900

    actvie_menu & scroll smooth

--- src/active_menu.js
+++ src/active_menu.js
@@ -17,8 +17,12 @@ const navItems = sectionIds.map((id) =>
 // console.log(navItems);
 
 const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -30,7 +34,7 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
     // console.log(index);
     // console.log(entry.target.id);
     // console.log(entry.isIntersecting);
@@ -43,10 +47,19 @@ function observerCallback(entries) {
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-    console.log(sectionIds[navIndex]);
+    // console.log(sectionIds[navIndex]);
+    selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections){
     const index = intersections.indexOf(true);
     return index     >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+    const navItem = navItems[index];
+    if(!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
 }
